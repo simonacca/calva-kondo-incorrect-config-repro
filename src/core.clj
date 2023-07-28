7a9aa3f8ff78222fd53aedf95555eac1689e4ee0
Simon Accascina <simon@accascina.me>
2023-07-28 15:25:50 +0200

    update repro

--- src/core.clj
+++ src/core.clj
@@ -4,3 +4,7 @@
 
 ; would expect this to be linted with `invalid-arity`
 (assoc)
+
+; try uncommenting this form while keeping the incorrect clj-kondo config.
+; you will find that clj-kondo starts working again (i.e. the `assoc` form gets linted)
+#_(go)
